a6703ebb25c34aec687e8d9e9b84292f9b186ee0
Özlem Arslan <111310126+ozlemars@users.noreply.github.com>
2024-10-04 14:40:22 +0300

    Add files via upload

---
+++ TemelProgramlama/1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*C#’da veri türleri temel olarak değer (value) ve referans (reference) olmak üzere ikiye ayrılır.
+ * Değer türleri (int, float, double, uint, struct vb) belleğin stack bölgesinde tutulur.
+ * Referans türleri (class, object, array, interface, delegate, string vb) belleğin heap bölgesinde tutulur.
+ * Metotlara veya metotlardan değer tiplerin referanslarını iletmek veya döndürmek için kullanılır. Diğer bir deyişle, referans yoluyla iletilen bir değerde yapılan herhangi bir değişikliğin, yalnızca taşıdığı değeri değil, adresteki (referans) değeriğinide değiştirdiğiniz için bu değişikliği yansıtacağı anlamına gelir. 
+ * ref  -> refrence : referans
+ */
+
+int x = 10;
+int y = 20;
+Console.WriteLine("{0},{1}",x,y);
+Degistir(x,y);
+Console.WriteLine("{0},{1}", x,y);
+Console.ReadKey(); //Kullanıcı tarafından basılan bir sonraki karakteri veya işlev tuşunu alır. Basılan tuş konsol penceresinde görüntülenir.
+
+
+static void Degistir(int a, int b)
+{
+    int gecici = a;
+    a = b;
+    Console.WriteLine("{0},{1}",a,b);
+}
---
+++ TemelProgramlama/2/Program.cs
@@ -0,0 +1,29 @@
+﻿var sehirler = new List<string>() //liste tanımı
+{
+    "Ankara" ,"istanbul", "Samsun", "Mardin", "Antalya"
+};
+/*foreach (string s in sehirler)
+{
+    Console.WriteLine(s); //girilen tüm şehirleri console yazdırır.
+} */
+
+//Lambda Expression =>
+sehirler.ForEach(s => Console.WriteLine(s));
+
+var iller = sehirler; //örtürülü değişken 
+iller.ForEach(i => Console.WriteLine(i));
+
+sehirler.Add("Sinop"); //şehirlere yeni liste elemanı ekleme
+sehirler.ForEach(s => Console.WriteLine(s));
+Console.WriteLine();
+iller.ForEach(i => Console.WriteLine(i));
+
+iller.Remove("Ankara");
+iller.ForEach(i => Console.WriteLine(i));
+
+Console.WriteLine();
+sehirler.ForEach(s => Console.WriteLine(s));
+
+
+
+Console.ReadKey();
